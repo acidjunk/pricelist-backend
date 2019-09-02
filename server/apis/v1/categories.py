4adc1049551b033a10a904cc8bb202d682c6b5ee
Rene Dohmen <acidjunk@gmail.com>
2019-09-02 09:27:34 +0200

    Added metat data to category and product endpoint (backwards incompatible)

--- server/apis/v1/categories.py
+++ server/apis/v1/categories.py
@@ -1,12 +1,12 @@
 import structlog
-from database import Category
+from database import Category, Shop
 from flask_restplus import Namespace, Resource, fields, marshal_with
 
 logger = structlog.get_logger(__name__)
 
 api = Namespace("categories", description="Shop category related operations")
 
-category_serializer = api.model(
+category_marshaller = api.model(
     "Category",
     {
         "id": fields.String(required=True),
@@ -15,12 +15,19 @@ category_serializer = api.model(
         "shop": fields.String(required=True, description="Shop name"),
     },
 )
+shop_marshaller = api.model("Shop", {"id": fields.String, "name": fields.String})
+
+category_list_fields = {
+    "shop": fields.Nested(shop_marshaller),
+    "categories": fields.List(fields.Nested(category_marshaller)),
+}
 
 
 @api.route("/shop/<shop_id>")
 @api.doc("Show all categories belonging to provided shop_id.")
 class CategoryResourceList(Resource):
-    @marshal_with(category_serializer)
+    @marshal_with(category_list_fields)
     def get(self, shop_id):
         # Todo: return categories from selected shop
-        return Category.query.filter_by(shop_id=shop_id).all()
+        shop = Shop.query.filter_by(id=shop_id).first()
+        return {"shop": shop, "categories": Category.query.filter_by(shop_id=shop_id).all()}
